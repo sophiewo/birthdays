5e6b1ff512335547d353667ff5170caab11f6f38
Sophie Worsnop <sophie.g.worsnop@gmail.com>
2020-05-13 11:01:59 +0100

    add_name method created

--- lib/birthday_list.rb
+++ lib/birthday_list.rb
@@ -1,1 +1,17 @@
 
+class BirthdayList
+
+  def initialize
+    birthday_list = []
+  end
+
+  def add_name(name, birthday)
+    birthday_list = []
+    birthday_list << {name: "Sophie", birthday: "07.08.1991"}
+    p birthday_list
+  end
+end
+
+# def print_names
+
+# def check_date
--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -1,1 +1,16 @@
+require 'birthday_list'
+
+describe BirthdayList do
+  subject(:birthday) { described_class.new }
+
+  it 'checks instance of class exists' do
+    expect(BirthdayList).to respond_to(:new)
+  end
+
+  describe '#add_name' do
+    it "should add a friend's name and birthday to list" do  
+      expect(subject.add_name("Sophie", "07.08.1991")).to eq [{name: 'Sophie', birthday: "07.08.1991"} ]
+    end
+  end
+end
 
